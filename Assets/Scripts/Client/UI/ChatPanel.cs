e6ab019991e22ceb7d625a1838db371f917a82c1
dkestell <drew.kestell@gmail.com>
2022-05-31 14:54:17 -0700

    WIP

---
+++ Assets/Scripts/Client/UI/ChatPanel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ChatPanel : MonoBehaviour
+{
+    [SerializeField] private RectTransform scrollableContent;
+
+    private void Awake()
+    {
+       // TODO: pubsub
+    }
+}
